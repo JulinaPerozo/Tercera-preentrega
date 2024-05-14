19defdf4cbe6c7d7afb38eaff29a19050321d3cb
Julina Perozo <julina333@hotmail.com>
2024-05-14 18:35:07 -0300

    feat: creación de modelo

--- Fiambreria/Clientes/models.py
+++ Fiambreria/Clientes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Sucursal(models.Model):
+    nombre = models.CharField(max_length=255)
+    direccion = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return f"{self.nombre}, {self.direccion}"
+
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    email = models.CharField(max_length=50)
+    sucursal = models.ForeignKey(Sucursal, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.nombre}, {self.apellido}, {self.email}"
+
+class Empleados(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    sucursal = models.ForeignKey(Sucursal, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.nombre}, {self.apellido}"
